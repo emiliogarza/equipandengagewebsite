48f8b989bfafab19933f856297ed7a1722a8dc53
Emilio Garza <emiliogarza07@gmail.com.com>
2024-01-03 11:39:51 -0800

    Updating packets and partner form

--- src/app/beyond/page.tsx
+++ src/app/beyond/page.tsx
@@ -562,7 +562,7 @@ function Resources() {
                 </h2>
               </FadeIn>
               <div className="lg:col-span-3">
-              <Link href="https://drive.google.com/file/d/1jeE1TaeKFc1xhnqjyTYP4fyw2KQ_g7Br/view?usp=share_link" target="_blank" className='block'>
+              <Link href="https://drive.google.com/file/d/1BA8eHnqmgk0EO5jv1Dxk6pwR7tXWKF3V/view?usp=sharing" target="_blank" className='block'>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 inline">
                   <path fillRule="evenodd" d="M9.75 6.75h-3a3 3 0 00-3 3v7.5a3 3 0 003 3h7.5a3 3 0 003-3v-7.5a3 3 0 00-3-3h-3V1.5a.75.75 0 00-1.5 0v5.25zm0 0h1.5v5.69l1.72-1.72a.75.75 0 111.06 1.06l-3 3a.75.75 0 01-1.06 0l-3-3a.75.75 0 111.06-1.06l1.72 1.72V6.75z" clipRule="evenodd" />
                   <path d="M7.151 21.75a2.999 2.999 0 002.599 1.5h7.5a3 3 0 003-3v-7.5c0-1.11-.603-2.08-1.5-2.599v7.099a4.5 4.5 0 01-4.5 4.5H7.151z" />
@@ -576,7 +576,7 @@ function Resources() {
                 </svg>
                 <span className='align-text-top'>&nbsp;Beyond Poster</span>
               </Link>
-              <Link href="https://drive.google.com/file/d/1SUhVB0cHLQfT7Nzoxsb6CogKP3SitY0Y/view?usp=share_link" target="_blank" className='block'>
+              <Link href="https://drive.google.com/file/d/1K5MyUW8ag3Wg2qU5xSLf-0cOfzij-gDC/view?usp=sharing" target="_blank" className='block'>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 inline">
                   <path fillRule="evenodd" d="M9.75 6.75h-3a3 3 0 00-3 3v7.5a3 3 0 003 3h7.5a3 3 0 003-3v-7.5a3 3 0 00-3-3h-3V1.5a.75.75 0 00-1.5 0v5.25zm0 0h1.5v5.69l1.72-1.72a.75.75 0 111.06 1.06l-3 3a.75.75 0 01-1.06 0l-3-3a.75.75 0 111.06-1.06l1.72 1.72V6.75z" clipRule="evenodd" />
                   <path d="M7.151 21.75a2.999 2.999 0 002.599 1.5h7.5a3 3 0 003-3v-7.5c0-1.11-.603-2.08-1.5-2.599v7.099a4.5 4.5 0 01-4.5 4.5H7.151z" />
